b963e1c5d1acf5eb15742f14ac80b4b48ae23782
BabuRoyC <prcbubu@gmail.com>
2023-05-01 01:09:11 +0530

    Made the simple first page

--- app/src/main/java/com/example/quzzeria/MainActivity.java
+++ app/src/main/java/com/example/quzzeria/MainActivity.java
@@ -1,11 +1,44 @@
 package com.example.quzzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
+    private void selectButton(AppCompatButton selectedButton)
+    {
+        String text = String.valueOf(selectedButton.getText());
+
+        switch (text)
+        {
+            case "Start New" : {
+
+                selectedButton.setOnClickListener(new View.OnClickListener()
+                {
+                    final AppCompatEditText name = findViewById(R.id.nameOfThePlayer);
+                    @Override
+                    public void onClick(View view)
+                    {
+                        name.setVisibility(View.VISIBLE);
+
+                        Intent gamePage = new Intent(MainActivity.this, GamePage.class);
+                        gamePage.putExtra("name", name.getText());
+                    }
+                });
+
+            }
+
+            case "LeaderBoard" : {
+
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
